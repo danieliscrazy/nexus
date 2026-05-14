de85cc811a08dbc1c9ced082e4467da8765a2465
techpixel <68567672+techpixel@users.noreply.github.com>
2026-05-14 12:59:06 -0400

    feat: opengraph

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 
 export default defineConfig({
+  site: 'https://nexus.hackclub.com',
   integrations: [svelte()],
 });
