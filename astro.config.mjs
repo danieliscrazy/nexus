7d25ea662a7f66da71a0b6b0e009bf9c30a95250
techpixel <68567672+techpixel@users.noreply.github.com>
2026-05-20 19:12:17 -0400

    feat: first anim

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,12 @@ import svelte from '@astrojs/svelte';
 export default defineConfig({
   site: 'https://nexus.hackclub.com',
   integrations: [svelte()],
+  vite: {
+    server: {
+      allowedHosts: ['.ts.net', '.ngrok-free.app', '.ngrok.app', '.ngrok.io'],
+    },
+    preview: {
+      allowedHosts: ['.ts.net', '.ngrok-free.app', '.ngrok.app', '.ngrok.io'],
+    },
+  },
 });
